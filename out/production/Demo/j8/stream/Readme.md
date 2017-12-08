b5661e98431c156f47f6f47cf1dad6bec7a5c34e
parane <psaravanaperumal@accelaero.com>
2017-12-08 17:30:24 +0530

    functionComposition added

---
+++ out/production/Demo/j8/stream/Readme.md
@@ -0,0 +1,11 @@
+Java Stream
+A new java.util.stream has been added in Java 8 to perform
+filter
+map
+reduce like operations with the collection.
+
+Stream API will allow sequential as well as parallel execution.
+
+This is one of the best feature for me because
+I work a lot with Collections and usually with Big Data,
+we need to filter out them based on some conditions.
--- src/j8/stream/Main.java
+++ src/j8/stream/Main.java
@@ -1,9 +1,31 @@
 package j8.stream;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static  void main(String[] a){
-        System.out.println("Hello world");
+        playWithStream();
+    }
+
+    public static void playWithStream(){
+        List<Integer> myList = new ArrayList<>();
+        for(int i=0; i<100; i++) myList.add(i);
+
+        //sequential stream
+        Stream<Integer> sequentialStream = myList.stream();
+
+        //parallel stream
+        Stream<Integer> parallelStream = myList.parallelStream();
+
+        //filter below 3, and double it and sum up !!!
+        System.out.println(sequentialStream
+                .filter(i -> i<3)
+                .map(i->i*2)
+               /* .forEach(System.out::println)*/
+                .reduce(0,(sum,i)->sum+i));
 
     }
 }
